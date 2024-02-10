c9453594c1cd35cc9a60ec0bc606aa1603042311
MrRoblick <takenpa20@gmail.com>
2024-02-10 09:55:32 +0300

    added src folder

---
+++ README.md
@@ -0,0 +1,3 @@
+SimpleClass is a module, that helps make classes with zero expierence. It also can helps with make class faster.
+
+SimpleClass is not recommended you for make class with expierence. Better use default method
---
+++ SimpleClass.lua
@@ -0,0 +1,34 @@
+local module = {}
+module.__index = module
+
+local trove = require(script.Trove)
+
+function module.new()
+	local object = setmetatable({},module)	
+	object.__index = object
+	
+	function object.new()
+		local self = setmetatable({},object)
+		
+		return self:Constructor() or self:Init() or self;
+	end
+	
+	return object:_init() or object;
+end
+
+
+function module:_init()
+	self._trove = trove.new()
+end
+
+
+function module:Destroy()
+	self._trove:Destroy()
+end
+
+function module:Init()
+end
+function module:Constructor()
+end
+
+return module;
