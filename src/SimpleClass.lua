4340b20c760c61f7334d4c881bcd2bc012c77323
MrRoblick <takenpa20@gmail.com>
2024-02-21 12:33:56 +0300

    cleaned module

--- src/SimpleClass.lua
+++ src/SimpleClass.lua
@@ -1,72 +1,49 @@
 local module = {}
 module.__index = module
 
-local gc_references = {
-	['trove'] = script.Trove,
-	['janitor'] = script.Janitor,
-	['maid'] = script.Maid}
-
 local gc_name = '_gccleaner'
 
-function module.new(cleanerName : string, isOnBasedClass : boolean) : module
-	cleanerName = cleanerName or 'Trove';
-	if isOnBasedClass then
-		return module.BasedCreate(cleanerName);
+local trove = require(script.Trove)
+
+function module.new() : module
+	local class = {}
+	class.__index = class
+
+	function class.new(...)
+		local self = setmetatable({},class)
+
+		return self:Constructor(...) or self;
 	end
-	if not gc_references[string.lower(cleanerName)] then
-		return;
-	end
+
+	function class:Constructor(...) end
+	class.extend = module.extend
 	
-	return module.CreateClass(cleanerName);
+	return class;
 end
 
-function module.CreateClass(cleanerName : string) : module
-	local object = {}
+function module:extend()
+	local super = self
+	
+	local object = setmetatable({},{__index = super})
 	object.__index = object
-
+	
 	function object.new(...)
 		local self = setmetatable({},object)
-
-		return self:_initGC(cleanerName) or self:Constructor(...) or self:Init() or self;
+		
+		return self:Constructor(...) or self;
 	end
-
-	function object:Constructor(...) end
-	function object:Init() end
-
-	function object:_initGC(name : string) : {}
-		local moduleRef = gc_references[string.lower(name)]
-		local module = require(moduleRef)
-
-		self[gc_name] = module.new()
+	
+	function object:SuConstructor(...)
+		super.Constructor(self,...)
+	end
+	
+	function object:Constructor(...)
+		self:SuConstructor(...)
+		
+		--other
 	end
 	
 	return object;
 end
-
-function module.BasedCreate(cleanerName : string) : module
-	local object = setmetatable({},module)	
-	object.__index = object
-
-	function object.new(...) : object
-		local self = setmetatable({},object)
-
-		return self:_initGC(cleanerName) or self:Constructor(...) or self:Init() or self;
-	end
-
-	return object;
-end
-function module:_initGC(name : name) : nil
-	local moduleRef = gc_references[string.lower(name)]
-	local module = require(moduleRef)
-	
-	self[gc_name] = module.new()
-end
-
-function module:Destroy()
-	self[gc_name]:Destroy()
-end
-
-function module:Init() end
-function module:Constructor() end
 
 return module;
